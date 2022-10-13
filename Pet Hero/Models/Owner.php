54450d3642746061a3dc480eee8f8f02a2e0b021
Ignacio Rios <ignaciorios_g@hotmail.com>
2022-10-13 14:55:13 -0300

    Modelos Owner Keeper, y Dog
    con sus respectivos getters y setters

---
+++ Pet Hero/Models/Owner.php
@@ -0,0 +1,64 @@
+<?php
+    namespace Models;
+
+    class Owner
+    {
+        private $id;
+        private $name;
+        private $surname;
+        private $sex;
+        private $dni;
+
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        public function setId($id)
+        {
+            $this->id = $id;
+        }
+
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        public function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        public function getSurname()
+        {
+            return $this->surname;
+        }
+
+        public function setSurname($surname)
+        {
+            $this->surname = $surname;
+        }
+
+        public function getSex()
+        {
+            return $this->sex;
+        }
+
+        public function setSex($sex)
+        {
+            $this->sex = $sex;
+        }
+
+        public function getDni()
+        {
+            return $this->dni;
+        }
+
+        public function setDni($dni)
+        {
+            $this->dni = $dni;
+        }
+        
+    }
+
+?>
