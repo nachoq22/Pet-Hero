b7cfef43d3a4cc78f34a53ddc17d4b45a902d697
Ignacio Rios <ignaciorios_g@hotmail.com>
2022-10-20 19:53:14 -0300

    cambios minimos, preparandos vistas

--- PetHero/Views/Home.php
+++ PetHero/Views/Home.php
@@ -4,44 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pet Hero :D</title>
 </head>
 <body>
-    Bienvenido
-    <form action="<?php echo FRONT_ROOT."/Location/Add" ?>" method="post" style="background-color: #EAEDED;padding: 2rem !important;">
-          <table> 
-            <thead>              
-              <tr>
-                <th>Adress</th>
-                <th>Neighborhood</th>
-                <th>city</th>
-                <th>province</th>
-                <th>Country</th>                
-              </tr>
-            </thead>
+    Bienvenido a pet hero, aqui podra buscar a un keeper adecuado para cuidar a su mascota asi como ofrecerse a cuidar otras mascotas!
 
-              <tr>
-                <td style="max-width: 120px;">    
-                  <input type="text" name="Adress" required>
-                </td>
-                <td>
-                  <input type="text" name="Neighborhood"  required>
-                </td>
-                <td>
-                  <input type="text" name="City" required>
-                </td>
-                <td>
-                  <input type="text" name="Province" required>
-                </td>                
-                <td>
-                  <input type="text" name="Country" required>
-                </td>            
-              </tr>
+    <a href="Login.php">Convertite en Owner!</a>
 
-          </table>
-          <div>
-            <input type="submit" class="btn" value="Agregar" style="background-color:#DC8E47;color:white;"/>
-          </div>
-        </form>
+
 </body>
 </html>
